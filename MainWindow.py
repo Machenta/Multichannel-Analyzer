08a4ed4775d8470856addefcb25eb1d26cbe51ec
Machenta <66017341+Machenta@users.noreply.github.com>
2023-01-31 21:14:00 +0000

    New user interface and new backend WIP

--- MainWindow.py
+++ MainWindow.py
@@ -27,7 +27,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1272, 1000)
+        MainWindow.resize(1300, 1000)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.ToolbarWidget = QtWidgets.QWidget(self.centralwidget)
